6a309445b15199b423f642cff5111c6071a78941
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2018-12-04 18:40:41 +0200

    implement #calculateHashForBlock

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const CryptoJS = require('crypto-js');
 
 const Block = require('./block');
 
-const firstBlockData = [ new Date().getTime() / 1000, null ];
+const genesisData = [ new Date().getTime() / 1000, null ];
 
 const blockchain = [
-    new Block(0, '', ...firstBlockData, calculateHash.apply(null, [0, ...firstBlockData, null]))
+    new Block(0, '', ...genesisData, calculateHash.apply(null, [0, ...genesisData, null]))
 ];
 
 function calculateHash(index, previousHash, timestamp, data){
@@ -25,21 +25,26 @@ function getLatestBlock(){
     return blockchain[blockchain.length - 1];
 }
 
-// function isValidNewBlock(newBlock, previousBlock){
-//     if (previousBlock.index + 1 !== newBlock.index) {
-//         console.log('invalid index');
-//         return false;
-//     } else if (previousBlock.hash !== newBlock.previousHash) {
-//         console.log('invalid previoushash');
-//         return false;
-//     } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
-//         console.log('invalid hash: ' + calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
-//         return false;
-//     }
-//     return true;
-// };
+function calculateHashForBlock(block){
+    return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+}
+
+function isValidNewBlock(newBlock, previousBlock){
+    if (previousBlock.index + 1 !== newBlock.index) {
+        console.log('invalid index');
+        return false;
+    } else if (previousBlock.hash !== newBlock.previousHash) {
+        console.log('invalid previoushash');
+        return false;
+    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+        console.log('invalid hash: ' + calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
+        return false;
+    }
+    return true;
+};
 
 blockchain.push(generateNextBlock({ id: 'object-1', data: { title: 'ABC', description: 'lorem ipsum' }, action: 'add' }));
 
 console.log(JSON.stringify(blockchain, null, 2));
+console.log(isValidNewBlock(blockchain[1], blockchain[0]));
 
